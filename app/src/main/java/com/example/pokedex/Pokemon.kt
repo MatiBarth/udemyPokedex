459ab5c690e2161fd1efd17b33162e281f566965
Matias Ariel Barthelemi <mbarthelemi@payway.com.ar>
2023-12-19 19:04:06 -0300

    commit

---
+++ app/src/main/java/com/example/pokedex/Pokemon.kt
@@ -0,0 +1,16 @@
+package com.example.pokedex
+
+data class Pokemon(
+    val id: Long,
+    val name: String,
+    val hp: Int,
+    val attack: Int,
+    val defense: Int,
+    val speed: Int,
+    val type: PokemonType
+) {
+
+    enum class PokemonType {
+        GRASS, FIRE, WATER, FIGHTER, ELECTRIC
+    }
+}
